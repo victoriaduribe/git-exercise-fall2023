62cf39153fad1e2bbaeeb38c1e6aef59cfdce943
Victoria Uribe <60197700+victoriaduribe@users.noreply.github.com>
2023-12-08 11:57:38 -0700

    added descriptive comments

--- testing_CAS_598/triangle_test_1.py
+++ testing_CAS_598/triangle_test_1.py
@@ -1,11 +1,11 @@
-import unittest
-from triangle_def import Triangle
+import unittest 
+from triangle_def import Triangle # import class definition to be used for unit test 
 
 # define test
-class TestAreaofRectangle(unittest.TestCase):
+class TestAreaofRectangle(unittest.TestCase): # test area of triangle
     def runTest(self):
-        triangle = Triangle(4, 10)
-        self.assertEqual(triangle.get_area(), 20, "incorrect area")
+        triangle = Triangle(4, 10) # give inputs for imported class definition (base, height) of triangle
+        self.assertEqual(triangle.get_area(), 20, "incorrect area") # define test value and error message if fails
  
 # run test
-unittest.main()
+unittest.main() 
